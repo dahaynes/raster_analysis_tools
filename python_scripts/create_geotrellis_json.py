064e75a1c7c94b1b742a626e1a615c30ed894fdb
David Haynes <haynesd2@gmail.com>
2018-09-19 10:57:54 -0500

    updated python geotrellis functions

--- python_scripts/create_geotrellis_json.py
+++ python_scripts/create_geotrellis_json.py
@@ -1,15 +1,14 @@
 import json, os
 
-# theDirectory = "/home/david/shapefiles"
-# theJSONS = [r"%s/%s" % (root, f) for root, dirs, files in os.walk(theDirectory) for f in files if "geojson" in f]
+theDirectory = "/home/david/shapefiles"
+theJSONS = [r"%s/%s" % (root, f) for root, dirs, files in os.walk(theDirectory) for f in files if "geojson" in f]
 
-# #theFilePath = "/home/david/shapefiles/4326/states.geojson"
-# allSubdirectories = filter(os.path.isdir, [os.path.join(theDirectory,f) for f in os.listdir(theDirectory)])
-# crs = [s.split(r"")[-1] for s in allSubdirectories]
+#theFilePath = "/home/david/shapefiles/4326/states.geojson"
+allSubdirectories = filter(os.path.isdir, [os.path.join(theDirectory,f) for f in os.listdir(theDirectory)])
+crs = [s.split("/")[-1] for s in allSubdirectories]
 
-# vectorDatasets = [ (aJSON,coord) for coord in crs for aJSON in theJSONS if coord in aJSON]
+vectorDatasets = [ (aJSON,coord) for coord in crs for aJSON in theJSONS if coord in aJSON]
 
-vectorDatasets = [(r"C:\scidb\shapefiles\4326\regions.geojson", 4326), (r"C:\scidb\shapefiles\5070\regions.geojson", 5070)]
 for vector in vectorDatasets:
     #splitting the JSON filepath and coordinate systems
 
@@ -26,9 +25,8 @@ for vector in vectorDatasets:
 
                 myGeoJSON['features'][featureID]['id'] = int(myGeoJSON['features'][featureID]['properties']['ID'])
 
-        jsonName = theJSONPath.split("\\")[-1].split(".")[0]
-        jsonDirectory = r"\\".join(theJSONPath.split("\\")[:-1])
-        print(jsonDirectory)
-        outJSON = r"%s\%s_2.geojson" % (jsonDirectory, jsonName)
-        print(outJSON)
+        jsonName = theJSONPath.split("/")[-1].split(".")[0]
+        jsonDirectory = "/".join(theJSONPath.split("/")[:-1])
+        outJSON = "%s/%s_2.geojson" % (jsonDirectory, jsonName)
+        #print(outJSON)
         json.dump(myGeoJSON, open(outJSON, 'w'))
